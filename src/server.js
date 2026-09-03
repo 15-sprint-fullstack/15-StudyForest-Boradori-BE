8dd991b0e134c92870add2069a488d6d14f89f99
Rasol07 <poloa0709@gmail.com>
2026-09-03 11:48:34 +0900

    feat: logger middleware 구성

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { cors } from './middlewares/cors.js';
+import { cors } from './middlewares/cors.middleware.js';
+import { logger } from './middlewares/logger.middleware.js';
 import { router } from './routes/index.js';
 import { config } from '#config';
 
 const app = express();
 app.use(cors);
 app.use(express.json());
+app.use(logger);
+
 app.use('/', router);
 app.listen(config.PORT, () => {
   console.log(
