ff580319c90f93ac5a643bd2b51f9d6f4bf9a62b
Rasol07 <poloa0709@gmail.com>
2026-09-03 15:46:49 +0900

    fix: index.js 내에 넣어둔 테스트 코드 studies 라우터 쪽으로 변경

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { studiesRouter } from './studies.route.js';
 import { prisma } from '#db/prisma.js';
-import { createStudySchema } from '#schemas';
 
 export const router = express.Router();
 
@@ -11,18 +10,3 @@ router.get('/', async (_req, res) => {
 });
 
 router.use('/studies', studiesRouter);
-
-// zod 로 파싱해서 에러 핸들러 하는 부분 예시로 적어뒀습니다.
-// req.body에 적은 내용 형식이 검증하는 단계입니다.
-// 차후 validate가 확실히 정해지면 그 쪽으로 들어갑니다.
-
-// 예시로 넣어뒀으니 작업하실 때 사용하시고, 삭제 하시거나 주석으로 처리해주세요.
-router.post('/studies', async (req, res, next) => {
-  try {
-    const validated = createStudySchema.parse(req.body);
-    const study = await prisma.study.create({ data: validated });
-    res.json(study);
-  } catch (error) {
-    next(error);
-  }
-});
