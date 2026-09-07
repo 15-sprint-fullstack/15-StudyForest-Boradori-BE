3bc63ea1ba995de9bb7dfd1bdcc825eec7aafcba
danielfirst98 <danielfirst98@gmail.com>
2026-09-07 13:32:34 +0900

    Add swagger habit api

--- swagger/habit.docs.js
+++ swagger/habit.docs.js
@@ -8,20 +8,36 @@
  *     summary: 습관 조회
  *     parameters:
  *       - in: path
- *         name: test
+ *         name: studyId
  *         required: true
- *         schema:
- *           type: string
- *       - in: query
- *         name: test
- *         schema:
- *           type: string
- *       - in: query
- *         name: test
  *         schema:
  *           type: string
  *     responses:
  *       200:
  *         description: 조회 성공
+ *
+ */
+
+/**
+ * @swagger
+ * /studies/{studyId}/habit/{habitId}:
+ *   post:
+ *     tags:
+ *       - HabitRecord
+ *     summary: 습관 기록 생성
+ *     parameters:
+ *       - in: path
+ *         name: studyId
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: habitId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       201:
+ *         description: 생성 성공
  *
  */
