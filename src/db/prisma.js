bab9782c6ca33ad41869a03653be3b6fbd0032aa
danielfirst98 <danielfirst98@gmail.com>
2026-09-07 14:25:22 +0900

    Fix prisma client setting

--- src/db/prisma.js
+++ src/db/prisma.js
@@ -1,6 +1,6 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { config } from '#config';
-import { PrismaClient } from '#generated/prisma/client.ts';
+import { PrismaClient } from '#generated/prisma/client.js';
 
 const adapter = new PrismaPg({
   connectionString: config.DATABASE_URL,
