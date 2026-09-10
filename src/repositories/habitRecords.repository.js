ce7647bd85b9e94700ff6ff8668ebfa721752caf
Seunghyun <mona1222@naver.com>
2026-09-10 15:29:05 +0900

    fix: 오타 수정

--- src/repositories/habitRecords.repository.js
+++ src/repositories/habitRecords.repository.js
@@ -22,7 +22,7 @@ function findList(studyId, startDate, endDate) {
   const utcStartDate = fromZonedTime(startDate, 'Asia/Seoul');
   const utcEndDate = fromZonedTime(endDate, 'Asia/Seoul');
 
-  console.log('변환된시간:', utcStartDate, utcEndtDate);
+  console.log('변환된시간:', utcStartDate, utcEndDate);
 
   return prisma.habitRecord.findMany({
     where: {
