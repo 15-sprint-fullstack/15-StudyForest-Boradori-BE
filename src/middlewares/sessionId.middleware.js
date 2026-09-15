da860dc3b974e26f726fbe21d57966e8f3cd6601
Rasol07 <poloa0709@gmail.com>
2026-09-16 01:40:30 +0900

    chore: 세션 저장소 설정

--- src/middlewares/sessionId.middleware.js
+++ src/middlewares/sessionId.middleware.js
@@ -1,7 +1,18 @@
 import session from 'express-session';
 import { STUDY_IDLE_TIMEOUT } from '../constants/studyAccess.js';
+import connectPgSimple from 'connect-pg-simple';
 
+const PgSession = connectPgSimple(session);
+
+const sessionStore = new PgSession({
+  conString: process.env.DATABASE_URL,
+  tableName: 'session',
+  createTableIfMissing: true,
+});
+
+// 배포할 때 store 설정 넣어줌.
 export const sessionId = session({
+  store: sessionStore,
   name: 'study_sid',
   secret: process.env.SESSION_SECRET_KEY,
   resave: false,
